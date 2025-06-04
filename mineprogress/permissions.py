cf5f0b808c869c4cd4d985aff8db73166d3121e0
mareng2007 <46856522+mareng2007@users.noreply.github.com>
2025-06-05 00:17:22 +0700

    Update MineProgress permission group

--- mineprogress/permissions.py
+++ mineprogress/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 class IsMineProgressUser(permissions.BasePermission):
     """
-    ตรวจสอบว่า user อยู่ในกลุ่ม 'mineprogress_user' หรือไม่
+    ตรวจสอบว่า user อยู่ในกลุ่ม 'mineprogress_access' หรือไม่
     """
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='mineprogress_user').exists()
+        return request.user.groups.filter(name='mineprogress_access').exists()
